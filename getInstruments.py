9e26a429209970bb4759b3e7cd48406426773f5e
jbwolfe <jbwolfe@crimson.ua.edu>
2015-01-29 23:11:14 -0600

    Updated getpack script to dynamically work and retrieve instrument list.

--- getInstruments.py
+++ getInstruments.py
@@ -1,16 +1,39 @@
 import os
+import urllib.request
+from html.parser import HTMLParser
+
+class parseClass(HTMLParser):
+  def handle_starttag(self, tag, attrs):
+    if(tag == "a"):
+      instrument = attrs[0][1].split('.')[0]
+      
+      print("Preparing getpack workspace")
+      if not os.path.exists("./tempGetpackWorkspace"):
+        os.mkdir("./tempGetpackWorkspace")
+      os.chdir("./tempGetpackWorkspace")
+      os.system("rm -f *")
+
+      print("Downloading instrument sample pack: " + instrument)
+      os.system("getpack \'"+instrument+"\'")
+      print(instrument)
+
+      print("Cleaning up getpack workspace")
+      os.system("rm -f *")
+      os.chdir("..")
+      os.rmdir("./tempGetpackWorkspace")
+      print("Finished downloading instument sample pack: " + instrument + '\n')
+
 
 def main():
-  fp = open("index.html", "r")
-  instruments = fp.read().split()
+  url = "http://beastie.cs.ua.edu/songlib/samples/"
+  try:
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    text = data.decode('utf-8')
+  except:
+    print("Error: could not reach songlib sample library")
 
-    
-  os.mkdir("./temp")
-  os.chdir("./temp")
-  #for i in range(0, len(instruments), 1):
-    #os.system("sudo getpack \'"+instruments[i]+"\'")
-    #os.system("sudo rm *")
-  os.chdir("..")
-  os.rmdir("./temp")
+  parser = parseClass()
+  parser.feed(text.strip())
 
 main()
